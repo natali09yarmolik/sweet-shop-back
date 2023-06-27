dd5ad94b8b128cc1b3778299fa905334777d478f
natali09yarmolik <85163434+natali09yarmolik@users.noreply.github.com>
2023-06-27 11:37:10 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3010
+
+app.listen(port, () => {
+    console.log('Server start' + port)
+})
+app.use(function (req, res, next) {
+
+    const corsWhitelist = [
+        'http://localhost:3000',
+        'https://natali09yarmolik.github.io',
+        /*'http://localhost:3010/src/items'*/
+
+    ];
+    if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    }
+    next();
+});
+
+const items = require('../items.json')
+app.get('/src/items', (req, res) => {
+    res.json({data: items})
+})
